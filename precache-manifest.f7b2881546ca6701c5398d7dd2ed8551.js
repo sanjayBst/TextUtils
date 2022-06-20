2e241a238f35e2b6f3dc379618f8374fcbf8d6e6
sanjayBst <sanjaybisht442@gmail.com>
2022-06-20 17:27:15 +0530

    Updates

---
+++ precache-manifest.f7b2881546ca6701c5398d7dd2ed8551.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "177a20cda0f02d80963e",
+    "url": "/TextUtils/static/css/main.ad3b027d.chunk.css"
+  },
+  {
+    "revision": "177a20cda0f02d80963e",
+    "url": "/TextUtils/static/js/main.a5a6c5a6.chunk.js"
+  },
+  {
+    "revision": "2d68cddf3b6c4021ca03",
+    "url": "/TextUtils/static/js/runtime~main.76bc5b29.js"
+  },
+  {
+    "revision": "dbe85eefb7b0bc56bddb",
+    "url": "/TextUtils/static/css/2.452ae18a.chunk.css"
+  },
+  {
+    "revision": "dbe85eefb7b0bc56bddb",
+    "url": "/TextUtils/static/js/2.2c9b0a19.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/TextUtils/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "df362ae3d064ddce0488865c8153bca7",
+    "url": "/TextUtils/index.html"
+  }
+];
