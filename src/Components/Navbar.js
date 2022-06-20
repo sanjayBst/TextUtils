82bcc515cdd134daf1fd9153f03f90a006187752
sanjayBst <sanjaybisht442@gmail.com>
2022-06-20 17:34:18 +0530

    Enhancement

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -28,7 +28,7 @@ export default function Navbar(props) {
               <Link
                 className="nav-link active"
                 aria-current="page"
-                to="/"
+                to="/home"
               >
                 Home
               </Link>
